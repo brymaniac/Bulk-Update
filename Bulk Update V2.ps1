0355567e46e7f0c9746e7942e88081ccdc7be6ca
brymaniac <74693150+brymaniac@users.noreply.github.com>
2022-12-22 10:16:21 -0500

    Add files via upload

---
+++ Bulk Update V2.ps1
@@ -0,0 +1,56 @@
+Add-Type -assembly System.Windows.Forms
+Add-Type -AssemblyName PresentationFramework
+$main_form = New-Object System.Windows.Forms.Form
+$main_form.Text ='Manager Update'
+$main_form.Width = 600
+$main_form.Height = 400
+$main_form.AutoSize = $true
+$main_form.BackColor = “lightblue”
+$Button = New-Object System.Windows.Forms.Button
+$Button.Location = New-Object System.Drawing.Size(140,75)
+$Button.Size = New-Object System.Drawing.Size(300,150)
+$Button.Text = "Select CSV"
+$Button.Font = 'Microsoft Sans Serif,10'
+$main_form.Controls.Add($Button)
+$Button.Add_Click(
+    {
+$initialDirectory = [Environment]::GetFolderPath('Desktop')
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$OpenFileDialog.InitialDirectory = $initialDirectory
+$OpenFileDialog.Filter = 'csv files (*.csv)|*.csv'
+$OpenFileDialog.Multiselect = $false
+$response = $OpenFileDialog.ShowDialog( ) # $response can return OK or Cancel
+$Users = Import-CSV $OpenFileDialog.FileName
+Where-Object { ($_.PSObject.Properties | ForEach-Object {$_.Value}) -ne $null}
+if  ($response -eq 'OK') {
+    Start-Transcript -Path C:\Temp\Add-Managers-.log -Append
+    Import-Module ActiveDirectory
+$msgBoxInput = [System.Windows.MessageBox]::Show( "Do you  to update these users?", " Removal Confirmation", "YesNoCancel", "Warning" )}
+switch  ($msgBoxInput) {
+    'Yes' {
+        ForEach ($User in $Users) {
+            $ADUser  = Get-ADUser -Filter "displayname -eq '$($User.Employee)'"
+            $manager = Get-ADUser -Filter "displayname -eq '$($User.Manager)'"
+            $title = Get-ADUser -Filter "displayname -eq '$($User.$Title)'"
+            $department = Get-ADUser -Filter "displayname -eq '$($User.Department)'"
+            if ($ADUser -eq $null) {
+                Write-Host "$ADUser does not exist in AD" -ForegroundColor Red
+            }
+            } 
+            else {
+                Set-ADUser -manager $manager
+                Set-ADUser -title $title
+                Set-ADUser -department $department
+A               Write-Host "Update $ADUser to $manager, $title, $department" -ForeGroundColor Green
+             "Command Succeeded"
+             $main_form.Close()
+            }
+            }
+    'No'{
+        "Command Failed"
+        $main_form.Close()
+    }
+        }
+    }
+        ) 
+$main_form.ShowDialog()
